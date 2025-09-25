bfbeea686ff0bf491a83b55808ba157a0725d017
amalaffet <amal.laffet@esprit.tn>
2025-09-25 23:42:04 +0000

    domain models

---
+++ src/main/java/com/libon/mower/domain/Orientation.java
@@ -0,0 +1,22 @@
+package com.libon.mower.domain;
+
+public enum Orientation {
+    N(0, 1),   // North: x doesn't change, y increases by 1
+    E(1, 0),   // East:  x increases by 1, y doesn't change  
+    S(0, -1),  // South: x doesn't change, y decreases by 1
+    W(-1, 0);  // West:  x decreases by 1, y doesn't change
+
+    private final int xMovement;
+    private final int yMovement;
+    
+    private Orientation(int xMovement, int yMovement) {
+        this.xMovement = xMovement;
+        this.yMovement = yMovement;
+    }
+
+    public int getXMovement() { return xMovement; }
+    public int getYMovement() { return yMovement; }
+
+
+
+}
